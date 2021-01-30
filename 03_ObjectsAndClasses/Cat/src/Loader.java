71e15983412d77ffb50911565917e598ce1a8caf
Aleksandr Isaev <isu172@mail.ru>
2021-01-30 21:22:44 +0300

    Home work number 3.1: Class and object - cat experiments

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -3,8 +3,48 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
+        Cat vasya = new Cat();
+        Cat luka = new Cat();
+        Cat timka = new Cat();
+        Cat gylka = new Cat();
+        Cat umka = new Cat();
 
-        System.out.println(cat.getStatus());
+        double ammountOfFood = 0;
+
+
+
+
+        System.out.println("Vasya weight:" + "\t" + vasya.getWeight());
+        vasya.feed(2.0);
+        System.out.println("Vasya ate" + "\t" + vasya.getWeight());
+
+        System.out.println("Luka weight:" + "\t" + luka.getWeight());
+        luka.feed(30.2);
+        System.out.println("Luka ate" + "\t" + luka.getWeight());
+        luka.drink(9.2);
+        System.out.println("Luka ate" + "\t" + luka.getWeight());
+        System.out.println("Timka weight:" + "\t" + timka.getWeight());
+        System.out.println("Gulka weight:" + "\t" + gylka.getWeight());
+        System.out.println("Umka weight:" + "\t" + umka.getWeight());
+
+        while (true)
+        {
+            luka.feed(ammountOfFood);
+            ammountOfFood++;
+            if (luka.getStatus().equals("Exploded")) {
+               System.out.println("Luka weight after eating" + "\t" + luka.getWeight() + "\n Luka status:" + "\t" + luka.getStatus());
+              break;
+           }
+        }
+
+       while (true)
+       {
+           timka.meow();
+           if (timka.getStatus().equals("Dead"))
+           {
+               System.out.println("Timka status:" + "\t" + timka.getStatus());
+               break;
+           }
+       }
     }
 }
