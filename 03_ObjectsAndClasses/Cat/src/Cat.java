642c70f5ae05aef2766a0f82ea1e4f52f3b21049
Aleksandr Isaev <isu172@mail.ru>
2021-02-21 23:03:16 +0300

    Home work 3.5: Create obgects ano constructors

--- 03_ObjectsAndClasses/Cat/src/Cat.java
+++ 03_ObjectsAndClasses/Cat/src/Cat.java
@@ -1,3 +1,4 @@
+import java.lang.invoke.VarHandle;
 import java.util.Scanner;
 
 public class Cat
@@ -6,10 +7,14 @@ public class Cat
     private double weight;
     private double foodEaten;
     private static int count;
+    private String name;
 
     private static final double MIN_WEIGHT = 1000.0;
     private static final double MAX_WEIGHT = 9000.0;
     private static final int NUBMER_OF_EYES = 2;
+    private double minWeight;
+    private double maxWeight;
+
 
 
     boolean catAlive = getStatus().equals("Sleeping") || getStatus().equals("Playing");
@@ -17,7 +22,28 @@ public class Cat
     {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
+        minWeight = MIN_WEIGHT;
+        maxWeight = MAX_WEIGHT;
         count++;
+    }
+
+    public Cat (String name, Double weight)
+    {
+        this();
+        this.name = name;
+        if (name.equals("0"))
+        {
+            nameInput();
+        }
+        if (weight <= 0.0)
+        {
+            weigthInput();
+        }
+        else
+        {
+            this.weight = weight; //введённый вес в параметре Double weight
+            weigthComparison();
+        }
     }
 
     public void meow()
@@ -54,13 +80,16 @@ public class Cat
     {
         return weight;
     }
-
+    public String name()
+    {
+        return name;
+    }
     public String getStatus()
     {
-        if(weight < MIN_WEIGHT) {
+        if(weight < minWeight) {
             return "Dead";
         }
-        else if(weight > MAX_WEIGHT) {
+        else if(weight > maxWeight) {
             return "Exploded";
         }
         else if(weight > originWeight) {
@@ -94,7 +123,7 @@ public class Cat
         {
             catAlive = false;
             count --;
-            System.out.println("Cat dead " + count); //просто для понимания процесса расчета
+//            System.out.println("Cat dead " + count); //просто для понимания процесса расчета
         }
     }
     public void catColor (CatColor type)
@@ -102,4 +131,37 @@ public class Cat
         System.out.println(type);
     }
 
+    public void nameInput()
+    {
+        System.out.println("Create a cat name. Cat number: " + count);
+        Scanner scName = new Scanner(System.in);//ввод имени кота чере сканер.
+        name = scName.nextLine();/* .nextline() ввод через сканер параметром String.
+                                 В целом можно преобразовать в Int или Double.
+                                 Преобразовать вышло, но в с if не получилось подружить. Может позже попробую */
+    }
+    public void weigthInput()
+    {
+        System.out.println("Create a cat weight. Cat number: " + count);
+        Scanner scWeight = new Scanner(System.in);
+        if (scWeight.hasNextInt()) {
+            weight = scWeight.nextDouble();// .nextDouble() ввод через сканер параметром Double можно задать Int и другие.
+            weigthComparison();
+        } else {
+            System.out.println("You can't enter the weight of the cat in letters. The weight of the cat is create automatically");
+        }
+    }
+    public void weigthComparison() /*
+                                   сравнение веса, если они больше или меньше минимального, то на этапе генерации кота,
+                                   количесво котов не увеличивается, т.е. создаём три кота, а у одного из них вес вешел за пределы,
+                                   тогда финальный результат количества котов будет равен 2 */
+     {
+         boolean minWeightCat = weight < MIN_WEIGHT;
+         boolean maxWightCat = weight > MAX_WEIGHT;
+         if (maxWightCat || minWeightCat)
+         {
+         catNotAlive();
+         System.out.println(minWeightCat ? name + " " + getStatus() + "." + " Entered weight is less than the minimum":
+         name + " " + getStatus() + "." + " Entered weight is higher than the maximum");
+         }
+     }
 }
--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -5,61 +5,57 @@ public class Loader
     {
         Cat vasya = new Cat();
         Cat luka = new Cat();
-        luka.catColor(CatColor.Sandy);
+//        luka.catColor(CatColor.Sandy);
         Cat timka = new Cat();
         Cat gylka = new Cat();
         Cat umka = new Cat();
+        double ammountOfFood = 0;
+
+        System.out.println("Vasya weight:" + "\t" + vasya.getWeight());
+        System.out.println("Luka weight:" + "\t" + luka.getWeight());
+        luka.feed(2.2);
+        System.out.println("Luka ate" + "\t" + luka.getWeight());
+        luka.drink(9.2);
+        System.out.println("Luka ate" + "\t" + luka.getWeight());
+        System.out.println("Timka weight:" + "\t" + timka.getWeight());
+        System.out.println("Gulka weight:" + "\t" + gylka.getWeight());
+        System.out.println("Umka weight:" + "\t" + umka.getWeight());
+
+        while (true)
+        {
+            luka.feed(ammountOfFood);
+            ammountOfFood++;
+            if (luka.getStatus().equals("Exploded")) {
+               System.out.println("Luka weight after eating" + "\t" + luka.getWeight() + "\n Luka status:" + "\t" + luka.getStatus());
+              break;
+           }
+        }
+
+       while (!timka.getStatus().equals("Dead"))
+       {
+           timka.meow();
+       }
+
+       System.out.println("Timka status: " + timka.getStatus());
+
+       System.out.println("Timka eight: " + gylka.getWeight());
+       gylka.feed(150.0);
+       gylka.pee();
+       gylka.drink(250.0);
+       gylka.feed(100.0);
+       gylka.meow();
+       gylka.feed(100.0);
+
+       System.out.println("Gulka weight: " + gylka.getWeight());
+       System.out.println("Amount of food eaten: " + gylka.amountOfFoodEaten());
+       System.out.println("Gulka weight: " + gylka.getWeight());
+       gylka.pee();
+       gylka.feed(100.0);
+       gylka.pee();
+       System.out.println("Gulka weight: " + gylka.getWeight());
 
 
-//        double ammountOfFood = 0;
-//        System.out.println("Vasya weight:" + "\t" + vasya.getWeight());
-//
-//        System.out.println("Vasya ate" + "\t" + vasya.getWeight());
-//
-//        System.out.println("Luka weight:" + "\t" + luka.getWeight());
-//        luka.feed(2.2);
-//        System.out.println("Luka ate" + "\t" + luka.getWeight());
-//        luka.drink(9.2);
-//        System.out.println("Luka ate" + "\t" + luka.getWeight());
-//        System.out.println("Timka weight:" + "\t" + timka.getWeight());
-//        System.out.println("Gulka weight:" + "\t" + gylka.getWeight());
-//        System.out.println("Umka weight:" + "\t" + umka.getWeight());
-//
-//        while (true)
-//        {
-//            luka.feed(ammountOfFood);
-//            ammountOfFood++;
-//            if (luka.getStatus().equals("Exploded")) {
-//               System.out.println("Luka weight after eating" + "\t" + luka.getWeight() + "\n Luka status:" + "\t" + luka.getStatus());
-//              break;
-//           }
-//        }
-//
-//       while (!timka.getStatus().equals("Dead"))
-//       {
-//           timka.meow();
-//       }
-//
-//       System.out.println("Timka status: " + timka.getStatus());
-//
-//       System.out.println("Weight: " + gylka.getWeight());
-//       gylka.feed(150.0);
-//       gylka.pee();
-//       gylka.drink(250.0);
-//       gylka.feed(100.0);
-//       gylka.meow();
-//       gylka.feed(100.0);
-//
-//       System.out.println("Weight: " + gylka.getWeight());
-//       System.out.println("Amount of food eaten: " + gylka.amountOfFoodEaten());
-//       System.out.println("Weight: " + gylka.getWeight());
-//       gylka.pee();
-//       gylka.feed(100.0);
-//       gylka.pee();
-//       System.out.println("Weight: " + gylka.getWeight());
-////
-//
-//
+
         luka.feed(3000.0);
         luka.feed(3000.0);
         luka.feed(3000.0);
@@ -74,10 +70,28 @@ public class Loader
 
         System.out.println("Luka color " + CatColor.Red + "\n Luka weight after eating " + luka.getWeight() + "\n Luka status: " +  luka.getStatus());
 
-//        vasya.feed(300.0);
-//
-//        System.out.println("Vasya weight after eating " + vasya.getWeight() + "\n Vasya status: " +  vasya.getStatus());
-        System.out.println("Вывод значения с getCount: " + Cat.getCount());
+        vasya.feed(300.0);
 
+        System.out.println("Vasya weight after eating " + vasya.getWeight() + "\n Vasya status: " +  vasya.getStatus());
+
+
+        Cat a = new Cat("sdf",0.0);
+        System.out.println("Cat name: " + a.name() + "\n" +  "Cat weight: " + a.getWeight());
+        Cat b = new Cat("0",1322.0);
+        System.out.println("Cat name: " + b.name() + "\n" + "Cat weight: " + b.getWeight());
+        Cat generate = getKitten();
+        System.out.println("Cat name: " + generate.name() + "\n" + "Cat weight: " + generate.getWeight());
+        Cat generate2 = getKitten();
+        System.out.println("Cat name: " + generate2.name() + "\n" + "Cat weight: " + generate2.getWeight());
+        Cat generate3 = getKitten();
+        System.out.println("Cat name: " + generate3.name() + "\n" + "Cat weight: " + generate3.getWeight());
+
+//        System.out.println("Вывод значения с getCount: " + Cat.getCount());
+        System.out.println("Количество созданых котов: " + Cat.getCount());
+
+    }
+    private static Cat getKitten()
+    {
+        return new Cat("0", 1100.0);
     }
 }
