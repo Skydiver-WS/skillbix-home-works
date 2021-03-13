61baaaef7c2ab7864df46e22867580b6718d6585
Aleksandr Isaev <isu172@mail.ru>
2021-03-13 23:01:11 +0300

    Home work 4.2: Minimal and maximal values of numbers

---
+++ 04_NumbersStringsAndDates/homework_4.2/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        byte a = Byte.MAX_VALUE;
+        byte b = Byte.MIN_VALUE;
+        short c = Short.MAX_VALUE;
+        short d = Short.MIN_VALUE;
+        int e = Integer.MAX_VALUE;
+        int f = Integer.MIN_VALUE;
+        long g = Long.MAX_VALUE;
+        long h = Long.MIN_VALUE;
+        float i = Float.MAX_VALUE;
+        float j = Float.MIN_VALUE;
+        double k = Double.MAX_VALUE;
+        double l = Double.MIN_VALUE;
+        System.out.println("byte max: " + a + "\t" + "min: " + b + "\n" + "short max: " + c + "\t" + "min: " + d + "\n" +
+                "int max: " + e + "\t" + "min: " + f + "\n" + "long max: " + g + "\t" + "min: " + h + "\n" + "float max: " + i + "\t" + "min: " + j + "\n" +
+                "double max: " + k + "\t" + "min: " + l);
+    }
+}
