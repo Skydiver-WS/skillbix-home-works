de84d3d3607d022ae82134b87a3a003379a9040f
Aleksandr Isaev <isu172@mail.ru>
2021-03-18 21:53:24 +0300

    Home work 4.3: Geometry Calculator

--- 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/GeometryCalculator.java
+++ 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/GeometryCalculator.java
@@ -1,21 +1,36 @@
 public class GeometryCalculator {
     // метод должен использовать абсолютное значение radius
     public static double getCircleSquare(double radius) {
-        return 0;
+        return Math.PI * Math.pow(radius, 2);
     }
 
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius) {
-        return 0;
+        return (4 * Math.PI * Math.pow(Math.abs(radius), 3)) / 3;
     }
 
     public static boolean isTrianglePossible(double a, double b, double c) {
-        return false;
+        if (!((a + b) > c) || !((a + c) > b) || !((c + b) > a))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
-
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTrianglePossible, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
+        if (isTrianglePossible(a, b, c)) // выполнится только при условии, что метод isTrianglePossible = true, это достигается за счет проверки условий в методе
+        {
+            double semiPerimeter = (a + b + c) / 2;
+            double triangSquare = Math.sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+            return triangSquare;
+        }
+        else
+        {
+            return -1;
+        }
     }
 }
--- 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        GeometryCalculator one = new GeometryCalculator();
+
+        System.out.println(one.getTriangleSquare(0, 0, 0));
 
     }
 }
