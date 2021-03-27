0d52b8545c24c6c33d81aad8cbda886738fa7ba3
Aleksandr Isaev <isu172@mail.ru>
2021-03-27 18:07:06 +0300

    Home work 4.3: Box containers and Truks. One class, one method

--- 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -1,30 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-    private final static int MAX_BOX = 27;
-    private final static int MAX_CONTAINERS_IN_A_TRUCK = 12;
-    private static int boxCount = 7853360;
-    private static int containersCount;
-    private static int trucksCount;
-
-
-    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        String boxes = scanner.nextLine();
-        BoxContainersAndTruks convoy = new BoxContainersAndTruks();
-        convoy.boxCounter();
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
-
-        /*
-        Грузовик: 1
-            Контейнер: 1
-                Ящик: 1
-                Ящик: 2
-        Необходимо:
-        грузовиков - 1 шт.
-        контейнеров - 1 шт.
-        */
-    }
+	public static void main(String[] args) {
+		int MAX_BOX = 27;
+		int MAX_CONTAINERS_IN_A_TRUCK = 12;
+		int boxCount = 0;
+		int containersCount = 0;
+		int trucksCount = 0;
+		
+		Scanner inputOfTheNumberOfBox = new Scanner(System.in);
+		int box = inputOfTheNumberOfBox.nextInt();
+		boxCount = Math.max(box, 0); //выбирает максимальное значение
+		int numberOfBoxesEntered = boxCount; // переменная для установления предела цикла, оно равно количеству введённых коробок
+		for (int i = 1; i <= numberOfBoxesEntered; i++)
+		{
+			if ((boxCount % MAX_BOX) == 0 || i == 1)
+			{
+				if ((containersCount % MAX_CONTAINERS_IN_A_TRUCK) == 0)
+				{
+					trucksCount += 1;
+					System.out.println("Грузовик: " + trucksCount);
+				}
+				containersCount += 1;
+				System.out.println("\t" + "Контейнер: " + containersCount);
+			}
+			boxCount = i;
+			System.out.println("\t\t" + "Ящик: " + boxCount);
+		}
+		System.out.println("Необходимо:" + "\n" + "грузовиков - " + trucksCount + " шт." + "\n" + "контейнеров - " + containersCount + " шт.");
+	}
 }
