240c0ae8852f5aad0b5e36930ea0afb24fddb0ca
Aleksandr Isaev <isu172@mail.ru>
2021-05-08 14:12:42 +0300

    Home work 6.5: Correction of the additional task

--- 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -8,18 +8,6 @@ public class Main {
 	
 	public static String searchAndReplaceDiamonds(String text, String placeholder) {
 		// TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-		while (true) {
-			Pattern pattern = Pattern.compile("<.+?>");
-			Matcher matcher = pattern.matcher(text);
-			if (matcher.find()) {
-				int start = matcher.start();
-				int end = matcher.end();
-				text = text.replace(text.substring(start, end), placeholder);
-			}
-			else {
-				break;
-			}
-		}
-		return text;
+		return text.replaceAll("<.+?>", placeholder);
 	}
 }
