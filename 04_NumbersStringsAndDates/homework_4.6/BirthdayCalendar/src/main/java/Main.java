108c47baffe83fc215de09a98a4b9e0c06144dc1
Aleksandr Isaev <isu172@mail.ru>
2021-05-19 20:25:29 +0300

    Home Work 6.6: Class Calendar v.2

--- 04_NumbersStringsAndDates/homework_4.6/BirthdayCalendar/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.6/BirthdayCalendar/src/main/java/Main.java
@@ -2,35 +2,32 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		
-		int day = 31;
-		int month = 12;
-		int year = 1991;
-		System.out.println(collectBirthdays(year, month, day));
-	}
-	
-	public static String collectBirthdays(int year, int month, int day) {
-		
-		//TODO реализуйте метод для построения строки в следующем виде
-		//0 - 31.12.1990 - Mon
-		//1 - 31.12.1991 - Tue
-		month = month - 1;
-		Calendar calendar = new GregorianCalendar(year, month, day);
-		Calendar currentTime = new GregorianCalendar();
-		SimpleDateFormat dateFormat = new SimpleDateFormat(" - dd.MM.yyyy - EE", Locale.ENGLISH);
-		int count = 0;
-		String dateReturn = "";
-		while (true) {
-			if (calendar.getTime().after(currentTime.getTime())) {
-				break;
-			}
-			else if (calendar.get(Calendar.DAY_OF_MONTH) == day && calendar.get(Calendar.MONTH) == month) {
-				dateReturn += count++ + dateFormat.format(calendar.getTime()) + System.lineSeparator();
-			}
-			calendar.add(Calendar.DAY_OF_MONTH, 1);
-		}
-		return dateReturn;
-	}
+
+  public static void main(String[] args) {
+
+    int day = 31;
+    int month = 12;
+    int year = 1991;
+    System.out.println(collectBirthdays(year, month, day));
+  }
+
+  public static String collectBirthdays(int year, int month, int day) {
+
+    //TODO реализуйте метод для построения строки в следующем виде
+    //0 - 31.12.1990 - Mon
+    //1 - 31.12.1991 - Tue
+    month = month - 1;
+    Calendar calendar = new GregorianCalendar(year, month, day);
+    Calendar currentTime = new GregorianCalendar();
+    SimpleDateFormat dateFormat = new SimpleDateFormat(" - dd.MM.yyyy - EE", Locale.ENGLISH);
+    int count = 0;
+    String dateReturn = "";
+    while (!(calendar.getTime().after(currentTime.getTime()))) {
+      if (calendar.get(Calendar.DAY_OF_MONTH) == day && calendar.get(Calendar.MONTH) == month) {
+        dateReturn += count++ + dateFormat.format(calendar.getTime()) + System.lineSeparator();
+      }
+      calendar.add(Calendar.DAY_OF_MONTH, 1);
+    }
+    return dateReturn;
+  }
 }
