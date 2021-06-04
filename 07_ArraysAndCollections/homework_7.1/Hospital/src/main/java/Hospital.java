e3a33ffbaf8a97d2afbe12fa98068700e1a68fbd
Aleksandr Isaev <isu172@mail.ru>
2021-06-04 20:57:04 +0300

    Home work 7.1: Arrays. No additional task.

--- 07_ArraysAndCollections/homework_7.1/Hospital/src/main/java/Hospital.java
+++ 07_ArraysAndCollections/homework_7.1/Hospital/src/main/java/Hospital.java
@@ -1,23 +1,46 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
+
 public class Hospital {
+  private static final float LOW_LIMIT_POSITIVE_TEMP = (float) 36.2;
+  private static final float HIGH_LIMIT_POSITIVE_TEMP = (float) 36.9;
+  private static float[] temperatureGenerate;
+  private static float averageTemperature;
 
-    public static float[] generatePatientsTemperatures(int patientsCount) {
+  public static float[] generatePatientsTemperatures(int patientsCount) {
 
-        //TODO: напишите метод генерации массива температур пациентов
-
-        return new float[0];
+    //TODO: напишите метод генерации массива температур пациентов
+    float minTemp = 32;
+    float maxTemp = 40;
+    temperatureGenerate = new float[patientsCount];
+    //temperatureGenerate = new float[]{32.1F, 33.1F, 32.5F, 33.5F, 34.5F, 36.5F, 38.5F, 39.5F, 33.3F, 32.7F, 36.9F, 36.5F, 34.3F, 37.5F, 32.5F, 32.5F, 32.4F, 34.5F, 35.4F, 32.5F, 34.5F, 39.4F, 32.5F, 36.5F, 36.4F, 39.6F, 37.5F, 32.5F, 37.5F, 39.4F};
+    for (int i = 0; i < temperatureGenerate.length; i++) {
+      temperatureGenerate[i] = minTemp + (float) (Math.random()) * ((maxTemp) - minTemp);
+      averageTemperature += temperatureGenerate[i];
+      BigDecimal bigDecimal = BigDecimal.valueOf(temperatureGenerate[i]);
+      temperatureGenerate[i] = Float.parseFloat(String.valueOf(bigDecimal.setScale(1, RoundingMode.HALF_UP)));
     }
+    averageTemperature = averageTemperature / temperatureGenerate.length;
+    return temperatureGenerate;
+  }
 
-    public static String getReport(float[] temperatureData) {
+  public static String getReport(float[] temperatureData) {
         /*
         TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
             а также температуры всех пациентов.
         */
-
-        String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
-
-        return report;
+    String temperaturesPatient = Arrays.toString(temperatureGenerate);
+    temperaturesPatient = temperaturesPatient.replaceAll("[^\\d+\\s.]", "");
+    int numberOfHealthy = 0;
+    temperatureData = temperatureGenerate;
+    for (float j : temperatureData) {
+      if (j >= LOW_LIMIT_POSITIVE_TEMP && j <= HIGH_LIMIT_POSITIVE_TEMP) {
+        numberOfHealthy++;
+      }
     }
+    return "Температуры пациентов: " + temperaturesPatient +
+      "\nСредняя температура: " + Math.round(averageTemperature * 100.0) / 100.0 +
+      "\nКоличество здоровых: " + numberOfHealthy;
+  }
 }
--- 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/ReverseArray.java
+++ 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/ReverseArray.java
@@ -1,7 +1,12 @@
 public class ReverseArray {
-
-    //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
-    public static String[] reverse (String[] strings){
-        return strings;
+  //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
+  public static String[] reverse(String[] strings) {
+    for (int i = 0; i < strings.length / 2; i++) {
+      String revers = strings[i]; // получение содержания строки по номеру
+      System.out.println(revers + " " + i);
+      strings[i] = strings[strings.length - i - 1]; // присваивание нового номера в строке. strings[i] похоже служит для инициализации и не более
+      strings[strings.length - i - 1] = revers; // окончательный возврат строки и перемещение её в новую ячейку
     }
+    return strings;
+  }
 }
