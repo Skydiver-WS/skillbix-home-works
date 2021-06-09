78bb807e03e39c9e8219890e43289b13b6a9f0c5
Aleksandr Isaev <isu172@mail.ru>
2021-06-09 23:01:28 +0300

    Home work 7.1: Arrays. Add additional task and fixed hospital task.

--- 07_ArraysAndCollections/homework_7.1/Hospital/src/main/java/Hospital.java
+++ 07_ArraysAndCollections/homework_7.1/Hospital/src/main/java/Hospital.java
@@ -1,11 +1,8 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Arrays;
 
 public class Hospital {
   private static final float LOW_LIMIT_POSITIVE_TEMP = (float) 36.2;
   private static final float HIGH_LIMIT_POSITIVE_TEMP = (float) 36.9;
-  private static float[] temperatureGenerate;
   private static float averageTemperature;
 
   public static float[] generatePatientsTemperatures(int patientsCount) {
@@ -13,15 +10,10 @@ public class Hospital {
     //TODO: напишите метод генерации массива температур пациентов
     float minTemp = 32;
     float maxTemp = 40;
-    temperatureGenerate = new float[patientsCount];
-    //temperatureGenerate = new float[]{32.1F, 33.1F, 32.5F, 33.5F, 34.5F, 36.5F, 38.5F, 39.5F, 33.3F, 32.7F, 36.9F, 36.5F, 34.3F, 37.5F, 32.5F, 32.5F, 32.4F, 34.5F, 35.4F, 32.5F, 34.5F, 39.4F, 32.5F, 36.5F, 36.4F, 39.6F, 37.5F, 32.5F, 37.5F, 39.4F};
+    float[] temperatureGenerate = new float[patientsCount];
     for (int i = 0; i < temperatureGenerate.length; i++) {
       temperatureGenerate[i] = minTemp + (float) (Math.random()) * ((maxTemp) - minTemp);
-      averageTemperature += temperatureGenerate[i];
-      BigDecimal bigDecimal = BigDecimal.valueOf(temperatureGenerate[i]);
-      temperatureGenerate[i] = Float.parseFloat(String.valueOf(bigDecimal.setScale(1, RoundingMode.HALF_UP)));
     }
-    averageTemperature = averageTemperature / temperatureGenerate.length;
     return temperatureGenerate;
   }
 
@@ -30,17 +22,17 @@ public class Hospital {
         TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
             а также температуры всех пациентов.
         */
-    String temperaturesPatient = Arrays.toString(temperatureGenerate);
-    temperaturesPatient = temperaturesPatient.replaceAll("[^\\d+\\s.]", "");
     int numberOfHealthy = 0;
-    temperatureData = temperatureGenerate;
-    for (float j : temperatureData) {
-      if (j >= LOW_LIMIT_POSITIVE_TEMP && j <= HIGH_LIMIT_POSITIVE_TEMP) {
+    for (int j = 0; j < temperatureData.length; j++) {
+      averageTemperature += temperatureData[j];
+      temperatureData[j] = Float.parseFloat(String.format("%.1f", temperatureData[j]));
+      if (temperatureData[j] >= LOW_LIMIT_POSITIVE_TEMP && temperatureData[j] <= HIGH_LIMIT_POSITIVE_TEMP) {
         numberOfHealthy++;
       }
     }
-    return "Температуры пациентов: " + temperaturesPatient +
-      "\nСредняя температура: " + Math.round(averageTemperature * 100.0) / 100.0 +
+    averageTemperature = averageTemperature / temperatureData.length;
+    return "Температуры пациентов: " + Arrays.toString(temperatureData).replaceAll("[^\\d+\\s.]", "") +
+      "\nСредняя температура: " + String.format("%.2f", averageTemperature) +
       "\nКоличество здоровых: " + numberOfHealthy;
   }
 }
--- 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -1,14 +1,22 @@
 public class TwoDimensionalArray {
-    public static char symbol = 'X';
+  public static char symbol = 'X';
 
-    public static char[][] getTwoDimensionalArray(int size) {
-
-        //TODO: Написать метод, который создаст двумерный массив char заданного размера.
-        // массив должен содержать символ symbol по диагоналям, пример для size = 3
-        // [X,  , X]
-        // [ , X,  ]
-        // [X,  , X]
-
-        return new char[0][0];
+  public static char[][] getTwoDimensionalArray(int size) {
+    char[][] multiArray = new char[size][size];
+    for (int i = 0; i < multiArray.length; i++) {
+      for (int j = 0; j < multiArray[i].length; j++) {
+        if (i + j == multiArray.length - 1 || (i + j) == j * 2) {
+          multiArray[i][j] = symbol;
+        } else {
+          multiArray[i][j] = ' ';
+        }
+      }
     }
+    //TODO: Написать метод, который создаст двумерный массив char заданного размера.
+    // массив должен содержать символ symbol по диагоналям, пример для size = 3
+    // [X,  , X]
+    // [ , X,  ]
+    // [X,  , X]
+    return multiArray;
+  }
 }
