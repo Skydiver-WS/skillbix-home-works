e4f0864efe45a74ed20338c4eb7e462c7178092a
Aleksandr Isaev <isu172@mail.ru>
2021-07-20 20:13:07 +0300

    Home work 9.3 (old version 7.3): TreeSet. Email list.

--- 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/EmailList.java
+++ 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/EmailList.java
@@ -1,15 +1,25 @@
-import java.util.Collections;
-import java.util.List;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailList {
+  private final TreeSet<String> treeSet = new TreeSet<>();
 
-    public void add(String email) {
-        // TODO: валидный формат email добавляется
+  public void add(String email) {
+    // TODO: валидный формат email добавляется
+    Pattern pattern = Pattern.compile("\\w+@\\w+\\.\\w+");
+    Matcher matcher = pattern.matcher(email);
+    if (matcher.matches()) {
+      treeSet.add(email.toLowerCase());
     }
+  }
 
-    public List<String> getSortedEmails() {
-        // TODO: возвращается список электронных адресов в алфавитном порядке
-        return Collections.emptyList();
-    }
-
+  public TreeSet<String> getSortedEmails() {
+    // TODO: возвращается список электронных адресов в алфавитном порядке
+    return treeSet;
+  }
 }
+
+  //=====================================================================================================================================
+
+
--- 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/Main.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
+  public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
+  private static String inputText;
+  private static String text = "";
+  private final static String INPUT_ITEMS = "Для добавления email введите команду ADD и введите email  формате email@email.com" +
+    "\nДля вывода списка всех дел введите команду LIST" +
+    "\nДля выхода из программы введите команду EXIT";
     
     /* TODO:
         Пример вывода списка Email, после ввода команды LIST в консоль:
@@ -17,17 +24,73 @@ public class Main {
         "Неверный формат email"
     */
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("0")) {
-                break;
-            }
-            
-            //TODO: write code here
-            
+  public static void main(String[] args) {
+    //TODO: write code here
+    EmailList emailList = new EmailList();
+    System.out.println(INPUT_ITEMS);
+    while (true) {
+      setInputText();
+      if (inputText.matches("ADD.*")) {
+        inputText = inputText.replaceFirst("ADD\\s+", "").toLowerCase();
+        if (emailList.getSortedEmails().contains(inputText)) {
+          System.out.println("Данные email уже существует. Выберете один из предложенных или придумайте новый:");
+          generateListEmail(inputText);
+        } else {
+          emailList.add(inputText);
+          System.out.println(inputText.matches("\\w+@\\w+\\.\\w+") ? "Email " + inputText + " успешно добавлен." : WRONG_EMAIL_ANSWER);
         }
+      } else if (inputText.equals("LIST")) {
+        for (String treeSet : emailList.getSortedEmails()) {
+          System.out.println(treeSet);
+        }
+      } else if (inputText.equals("EXIT")) {
+        break;
+      }
     }
+  }
+
+  private static void setInputText() {
+    Scanner scanner = new Scanner(System.in);
+    inputText = scanner.nextLine();
+  }
+
+  private static void generateRandomEmail() {
+    int aCharCode = 97;
+    int zCharCode = 122;
+    int zeroCharCode = 48;
+    int nineCharCode = 57;
+    int countChar = (int) (1 + Math.random() * 10);
+    for (int i = 0; i < countChar; i++) {
+      int numberOrLetter = (int) (1 + Math.random() * 10);
+      char character;
+      if (numberOrLetter < 5) {
+        character = (char) (aCharCode + Math.random() * (zCharCode - aCharCode));
+      } else {
+        character = (char) (zeroCharCode + Math.random() * (nineCharCode - zeroCharCode));
+      }
+      text += String.valueOf(character);
+    }
+  }
+
+  private static void generateListEmail(String newEmail) {
+    ArrayList<String> optionsEmail = new ArrayList<>();
+    optionsEmail.trimToSize();
+
+    for (int i = 0; i < 5; i++) {
+      String[] splitEmail = newEmail.split("@");
+      ArrayList<String> wordsEmail = new ArrayList<>(Arrays.asList(splitEmail)); // почитать про asList
+      generateRandomEmail();
+      wordsEmail.add(1, text + "@");
+      text = "";
+      String email = "";
+      for (String s : wordsEmail) {
+        email += s;
+      }
+      optionsEmail.add(email);
+      wordsEmail.clear();
+      System.out.println(optionsEmail.get(i));
+    }
+    optionsEmail.clear();
+  }
 }
+
