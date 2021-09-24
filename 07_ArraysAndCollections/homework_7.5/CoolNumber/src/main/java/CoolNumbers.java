5990280c598259155becdd70b14ab24d02877cf0
Aleksandr Isaev <isu172@mail.ru>
2021-09-24 22:21:08 +0300

    Home work 9.5 (old version 7.5): Search and sort.

--- 07_ArraysAndCollections/homework_7.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 07_ArraysAndCollections/homework_7.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -1,29 +1,41 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
-
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+      HashSet<String> notDuplicateNumber = new HashSet<>();
+    String[] letters = {"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"};
+    int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    while (notDuplicateNumber.size() < 2000000) {
+      String number = "";
+      String firstLetters = letters[(int) (Math.random() * 12)];
+      for (int i = 0; i < 3; i++) {
+        number += numbers[(int) (Math.random() * 10)];
+      }
+      String secondLetters = letters[(int) (Math.random() * 12)] + letters[(int) (Math.random() * 12)];
+      int regionNumber = 1 + (int) (Math.random() * (199 - 1));
+      String regionNumberStr = regionNumber < 10 ? "0" + regionNumber : String.valueOf(regionNumber);
+      notDuplicateNumber.add(String.format("%s%s%s%s", firstLetters, number, secondLetters, regionNumberStr));
     }
+      return new ArrayList<>(notDuplicateNumber);
+  }
 
-    public static boolean bruteForceSearchInList(List<String> list, String number) {
-        return false;
-    }
+  public static boolean bruteForceSearchInList(List<String> list, String number) {
+    return list.contains(number);
+  }
 
-    public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
-    }
+  public static boolean binarySearchInList(List<String> sortedList, String number) {
+    Collections.sort(sortedList);
+    int index = Collections.binarySearch(sortedList, number);
+    return index >= 0;
+  }
 
 
-    public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
-    }
+  public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
+    return hashSet.contains(number);
+  }
 
-    public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
-    }
+  public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
+    return treeSet.contains(number);
+  }
 
 }
--- 07_ArraysAndCollections/homework_7.5/CoolNumber/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.5/CoolNumber/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Main {
     /*
     TODO:
@@ -6,7 +8,72 @@ public class Main {
      - проанализоровать полученные данные
      */
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    ArrayList<String> carNumberList = new ArrayList<>(CoolNumbers.generateCoolNumbers());
+    System.out.println(carNumberList);
+    for (int i = 0; i < 5; i++) {
+      String inputSearchNumber = carNumberList.get(1 + (int) (Math.random() * (carNumberList.size() - 1)));
+      var oneTestStart = System.nanoTime();
+      String testOne = CoolNumbers.bruteForceSearchInList(carNumberList, inputSearchNumber) ? "Поиск перебором: номер найден, " : "Поиск перебором: номер не найден, ";
+      var oneTestEnd = System.nanoTime();
+      //============================================================================================================================================================
+      var twoTestStart = System.nanoTime();
+      String testTwo = CoolNumbers.binarySearchInList(carNumberList, inputSearchNumber) ? "Бинарный поиск: номер найден, " : "Бинарный поиск: номер не найден, ";
+      var twoTestEnd = System.nanoTime();
+      //============================================================================================================================================================
+      var treeTestStart = System.nanoTime();
+      String testTree = CoolNumbers.searchInHashSet(new HashSet<>(carNumberList), inputSearchNumber) ? "Поиск в HashSet: номер найден, " : "Поиск в HashSet: номер не найден, ";
+      var treeTestEnd = System.nanoTime();
+      //============================================================================================================================================================
+      var fourTestStart = System.nanoTime();
+      String testFour = CoolNumbers.searchInTreeSet(new TreeSet<>(carNumberList), inputSearchNumber) ? "Поиск в TreeSet: номер найден, " : "Поиск в TreeSet: номер не найден, ";
+      var fourTestEnd = System.nanoTime();
+      //============================================================================================================================================================
+      var resultTestOne = oneTestEnd - oneTestStart;
+      var resultTestTwo = twoTestEnd - twoTestStart;
+      var resultTestTree = treeTestEnd - treeTestStart;
+      var resultTestFour = fourTestEnd - fourTestStart;
 
+      System.out.println(testOne + "занял " + resultTestOne + "нс" + "\n" +
+        testTwo + "занял  " + resultTestTwo + "нс" + "\n" +
+        testTree + "занял " + resultTestTree + "нс" + "\n" +
+        testFour + "занял " + resultTestFour + "нс" + "\n" +
+        Math.max(resultTestOne, Math.max(resultTestTwo, Math.max(resultTestTree, resultTestFour))) + "\n" +
+        Math.min(resultTestOne, Math.min(resultTestTwo, Math.min(resultTestTree, resultTestFour))) + "\n");
     }
+    searchUnkmnownNumber(carNumberList);
+  }
+
+  public static void searchUnkmnownNumber(List<String> carNumberList) {
+    System.out.println("Введите номер для поиска");
+    Scanner scanner = new Scanner(System.in);
+    String inputSearchNumber = scanner.nextLine();
+    var oneTestStart = System.nanoTime();
+    String testOne = CoolNumbers.bruteForceSearchInList(carNumberList, inputSearchNumber) ? "Поиск перебором: номер найден, " : "Поиск перебором: номер не найден, ";
+    var oneTestEnd = System.nanoTime();
+    //============================================================================================================================================================
+    var twoTestStart = System.nanoTime();
+    String testTwo = CoolNumbers.binarySearchInList(carNumberList, inputSearchNumber) ? "Бинарный поиск: номер найден, " : "Бинарный поиск: номер не найден, ";
+    var twoTestEnd = System.nanoTime();
+    //============================================================================================================================================================
+    var treeTestStart = System.nanoTime();
+    String testTree = CoolNumbers.searchInHashSet(new HashSet<>(carNumberList), inputSearchNumber) ? "Поиск в HashSet: номер найден, " : "Поиск в HashSet: номер не найден, ";
+    var treeTestEnd = System.nanoTime();
+    //============================================================================================================================================================
+    var fourTestStart = System.nanoTime();
+    String testFour = CoolNumbers.searchInTreeSet(new TreeSet<>(carNumberList), inputSearchNumber) ? "Поиск в TreeSet: номер найден, " : "Поиск в TreeSet: номер не найден, ";
+    var fourTestEnd = System.nanoTime();
+    //============================================================================================================================================================
+    var resultTestOne = oneTestEnd - oneTestStart;
+    var resultTestTwo = twoTestEnd - twoTestStart;
+    var resultTestTree = treeTestEnd - treeTestStart;
+    var resultTestFour = fourTestEnd - fourTestStart;
+
+    System.out.println(testOne + "занял " + resultTestOne + "нс" + "\n" +
+      testTwo + "занял  " + resultTestTwo + "нс" + "\n" +
+      testTree + "занял " + resultTestTree + "нс" + "\n" +
+      testFour + "занял " + resultTestFour + "нс" + "\n" +
+      Math.max(resultTestOne, Math.max(resultTestTwo, Math.max(resultTestTree, resultTestFour))) + "\n" +
+      Math.min(resultTestOne, Math.min(resultTestTwo, Math.min(resultTestTree, resultTestFour))) + "\n");
+  }
 }
